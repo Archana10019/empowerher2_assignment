55d35f6d3f36f41ae0fbb1cfffc8a85b2f6ae407
Archana <archanapal1910@gmail.com>
2025-11-26 22:04:22 +0530

    question19

---
+++ adv_js_assignment/question19-20/ques19/ques19.js
@@ -0,0 +1,16 @@
+//display the age 
+var age = 25;
+
+function displayAge(){
+    console.log("current age:" ,age)
+}
+
+function currentAge(){
+  age++;
+  console.log("current age",age);
+}
+
+displayAge();
+currentAge();
+displayAge();
+currentAge();
